f4ee95134b43c84a1fbc68ca55b15c84fec37250
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2023-01-27 00:29:33 +0100

    DragNDrop init component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,34 @@
 <template>
-  <h1></h1>
+  <p draggable="true" @dragstart="e => drag(e)">Click and drag me 👉</p>
+  <div class="abs-center">
+    <DragNDrop
+      class="bottom-24"
+      size="250px"
+      placeholder="drop here"
+      /> 
+  </div>
 </template>
 <script setup>
 
+//==============================
+// Imports
+//==============================
+import { computed, onMounted, reactive, ref } from "@vue/runtime-core";
+import DragNDrop from "../components/DragNDrop.vue";
+
+//==============================
+// Computed
+//==============================
+
+//==============================
+// Functions
+//==============================
+function drag(e) {
+  e.dataTransfer.setData("text", 'Voilà 😎');
+}
 </script>
+<style lang="scss" scoped>
+ p {
+  margin: 50px;
+ }
+</style>
